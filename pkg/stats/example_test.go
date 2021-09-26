e32d937353e95d8ef9b7b29df5fcc44178059bd8
Timofey Savenko <tim_savenko@go.alif.academy>
2021-09-26 20:02:58 +0300

    созданы функции модуля stats

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -1,1 +1,30 @@
 package stats
+
+import (
+	"fmt"
+
+	"github.com/nikolay2019-git/bank.git/pkg/bank/types"
+)
+
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{
+			ID:       0,
+			Amount:   1_000_00,
+			Category: "food",
+		},
+		{
+			ID:       1,
+			Amount:   2_000_00,
+			Category: "food",
+		},
+		{
+			ID:       2,
+			Amount:   3_000_00,
+			Category: "food",
+		},
+	}
+	fmt.Println(TotalInCategory(payments, "food"))
+	//Output:600000
+
+}
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,1 +1,18 @@
 package stats
+
+import "github.com/nikolay2019-git/bank.git/pkg/bank/types"
+
+//TotalInCategory находит сумму покупок в определённой категории
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+
+	//ToDo
+	sum := types.Money(0)
+	for _, payment := range payments {
+
+		if payment.Category == category {
+			sum += payment.Amount
+		}
+
+	}
+	return sum
+}
